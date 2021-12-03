263fd790a5a72318cd2091598bef8c84720491dd
ShibaYutta <73189830+ShibaYutta@users.noreply.github.com>
2021-12-03 18:30:48 -0300

    Day 3
    Update part

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -10,7 +10,7 @@ export const getPosts = () => async (dispatch) => {
   }
 };
 
-export const createPost = (post) => (dispatch) => {
+export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
 
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getPost } from "../controllers/post.js";
+import { getPost, createPost } from "../controllers/post.js";
 
 const router = express.Router();
 
 router.get("/", getPost);
+router.post("/", createPost);
+router.patch("/:id", updatePost);
 
 export default router;
